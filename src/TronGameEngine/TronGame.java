e45747eb3e4e0afce05ef091cfdf18d3e4d2268f
Mohamed R. Al-Bana <Mohamed.Bana@BusinessVision.Co>
2019-03-30 16:21:03 +0200

    Make Update methos in the Core class abstract and modify the gameLoop Method Create TronGame class and create its constructor to initialize the objects.

---
+++ src/TronGameEngine/TronGame.java
@@ -0,0 +1,72 @@
+package TronGameEngine;
+import Model.PointPosition;
+import Model.Core;
+import Model.MouseController;
+import Model.KeyboardController;
+import Model.Direction;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Window;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Mohamed
+ */
+public class TronGame extends Core{
+
+    
+    Player player1;
+    Player player2;
+    Player player3;
+    
+    KeyboardController keyboardController1;
+    KeyboardController keyboardController2;
+    KeyboardController keyboardController3;
+
+    MouseController  mouseController;
+
+    
+    ArrayList<Player> players;
+    
+    public TronGame(){
+    
+        
+        
+        player1 = new Player(new PointPosition(0, 0), Color.GREEN, Direction.Directions.RIGHT);
+        keyboardController1 = new KeyboardController(player1, KeyEvent.VK_UP, KeyEvent.VK_DOWN, KeyEvent.VK_RIGHT, KeyEvent.VK_LEFT);
+
+                
+        player2 = new Player(new PointPosition(1366, 600), Color.RED, Direction.Directions.LEFT);
+        keyboardController2 = new KeyboardController(player2, KeyEvent.VK_W, KeyEvent.VK_S, KeyEvent.VK_D, KeyEvent.VK_A);
+        
+        player3 = new Player(new PointPosition(0, 400), Color.YELLOW, Direction.Directions.RIGHT);
+        keyboardController3 = new KeyboardController(player3, KeyEvent.VK_NUMPAD8, KeyEvent.VK_NUMPAD2, KeyEvent.VK_NUMPAD6, KeyEvent.VK_NUMPAD4);
+        
+        
+        mouseController = new MouseController(player2);
+        
+        players = new ArrayList<>();
+        players.add(player1);
+        players.add(player2);
+        //players.add(player3);
+       
+        
+        
+
+
+    }
+    
+    
+    @Override
+    public void draw(Graphics2D g) {
+        
+    }
+
+    @Override
+    public void update() {
+        
+    }
+    
+}
