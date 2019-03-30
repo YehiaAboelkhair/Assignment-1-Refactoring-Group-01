4495b81585ff044a92fe60757d1ef2db421a87e0
Mohamed R. Al-Bana <Mohamed.Bana@BusinessVision.Co>
2019-03-30 16:01:59 +0200

    Create PointPosition class to specify the coordinations of each player.

---
+++ src/Model/PointPosition.java
@@ -0,0 +1,20 @@
+package Model;
+
+
+
+/**
+ *
+ * @author Mohamed
+ */
+public class PointPosition {
+    
+    public int xPosition; 
+    public int yPosition;
+
+        public PointPosition(int xPosition,int yPosition){
+    
+         this.xPosition = xPosition;
+         this.yPosition = yPosition;
+    }
+    
+}
