b0121d539e5054f21ad48b4b4bbe23a7b67d5763
Mohamed R. Al-Bana <Mohamed.Bana@BusinessVision.Co>
2019-03-30 16:31:39 +0200

    Remove yourclass class because it breaks many of the software quality principles. Create TronPlay class which contains the main method used to run the game.

---
+++ src/TronGameEngine/TronPlay.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TronGameEngine;
+
+/**
+ *
+ * @author Mohamed
+ */
+public class TronPlay {
+    
+    public static void main(String[] args) {
+        TronGame tg= new TronGame();
+        tg.run();
+    }
+    
+}
