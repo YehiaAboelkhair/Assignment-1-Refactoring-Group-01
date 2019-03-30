2a332d6d09e07a652c9a1ef4c6dd46b7f66dcf59
Mohamed R. Al-Bana <Mohamed.Bana@BusinessVision.Co>
2019-03-30 16:00:44 +0200

    Create Direction Class to specify the directions of players.

---
+++ src/Model/Direction.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Mohamed
+ */
+public class Direction {
+ 
+    public enum Directions { 
+
+        LEFT, UP, DOWN, RIGHT
+    };
+
+    
+}
